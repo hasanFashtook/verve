0545e536d487f472b92345721af0431e74d2928d
hasanFashtouk <hasanfashtook5000@gmail.com>
2024-03-20 03:54:13 +0300

    hard coded backend url

--- Front-End/app/_utils/instanceAxios.ts
+++ Front-End/app/_utils/instanceAxios.ts
@@ -5,7 +5,7 @@ const apiKey = process.env.NEXT_PUBLIC_STRAPI_APT;
 const backendUrl = process.env.NEXT_PUBLIC_BACK_END_URL;
 
 export const instanceAxios = axios.create({
-  baseURL: `${backendUrl}/api/`,
+  baseURL: `https://verve-admin-panel.onrender.com/api/`,
   headers: {
     Authorization: `Bearer ${apiKey}`
   }
